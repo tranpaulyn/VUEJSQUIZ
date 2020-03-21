0f41764969743015ee0419e7c5162ab978abb1a8
Paulyn Tran <paulyntran@Paulyns-Mac-mini.local>
2020-03-20 21:14:17 -0600

    Set up basic vue js and bootstrap framework for quiz

---
+++ quiz/src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <b-nav tabs>
+      <b-nav-item disabled><strong>Fancy Quiz App</strong></b-nav-item>
+      <b-nav-item disabled>Counter: ?/10</b-nav-item>
+    </b-nav>
+  </div>
+</template>
---
+++ quiz/src/components/QuestionBox.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="question-box-container">
+    <b-jumbotron>
+      <template v-slot:lead>
+        Some Question Here?
+      </template>
+
+      <hr class="my-4">
+
+      <p>
+        List of Answers
+      </p>
+
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button variant="success" href="#">Next</b-button>
+    </b-jumbotron>
+  </div>
+</template>
